f4195c016622a45d13ae609a799690ccbab9e3c1
Brajnn <olido22@wp.pl>
2024-06-21 07:55:18 +0200

    Adding search by request number for leave request

---
+++ Out_of_Office.Application/Project/ProjectDto.cs
@@ -0,0 +1,20 @@
+﻿using Out_of_Office.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Out_of_Office.Application.Project
+{
+    public class ProjectDto
+    {
+        public int ID { get; set; }
+        public string ProjectType { get; set; }
+        public DateTime StartDate { get; set; }
+        public DateTime? EndDate { get; set; }
+        public string ProjectManagerFullName { get; set; } 
+        public string? Comment { get; set; }
+        public ProjectStatus Status { get; set; }
+    }
+}
--- Out_of_Office/Controllers/LeaveRequestController.cs
+++ Out_of_Office/Controllers/LeaveRequestController.cs
@@ -1,5 +1,6 @@
 ﻿using MediatR;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.Data.SqlClient;
 using Out_of_Office.Application.Leave_Request.Query;
 using Out_of_Office.Application.Leave_Request.Query.GetAllLeaveRequest;
 using Out_of_Office.Application.Leave_Request.Query.GetLeaveRequestById;
@@ -16,15 +17,29 @@ namespace Out_of_Office.Controllers
         }
 
         [HttpGet]
-        public async Task<IActionResult> Index(int? SearchRequestId)
+        public async Task<IActionResult> Index(int? searchRequestId, string sortOrder)
         {
+            ViewBag.CurrentSort = sortOrder;
+            ViewBag.IdSortParm = string.IsNullOrEmpty(sortOrder) ? "id_desc" : "";
+            ViewBag.EmployeeSortParm = sortOrder == "employee_asc" ? "employee_desc" : "employee_asc";
+            ViewBag.StartDateSortParm = sortOrder == "startdate_asc" ? "startdate_desc" : "startdate_asc";
+
             var leaveRequests = await _mediator.Send(new GetAllLeaveRequestsQuery());
 
-            if (SearchRequestId.HasValue)
+            if (searchRequestId.HasValue)
             {
-                leaveRequests = leaveRequests.Where(lr => lr.ID == SearchRequestId.Value).ToList();
-                ViewData["SearchRequestId"] = SearchRequestId.Value;
+                leaveRequests = leaveRequests.Where(lr => lr.ID == searchRequestId.Value).ToList();
+                ViewData["SearchRequestId"] = searchRequestId.Value;
             }
+            leaveRequests = sortOrder switch
+            {
+                "id_desc" => leaveRequests.OrderByDescending(lr => lr.ID).ToList(),
+                "employee_asc" => leaveRequests.OrderBy(lr => lr.EmployeeFullName).ToList(),
+                "employee_desc" => leaveRequests.OrderByDescending(lr => lr.EmployeeFullName).ToList(),
+                "startdate_asc" => leaveRequests.OrderBy(lr => lr.StartDate).ToList(),
+                "startdate_desc" => leaveRequests.OrderByDescending(lr => lr.StartDate).ToList(),
+                _ => leaveRequests.OrderBy(lr => lr.ID).ToList(), 
+            };
 
             return View(leaveRequests);
         }
